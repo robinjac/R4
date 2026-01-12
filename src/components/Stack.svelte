6b84d8c723f8c5b09703630414d02879d061e1d2
robin jacobsson <robin23jac@gmail.com>
2026-01-12 14:53:46 +0100

    Remove stretch and set always align hoizontally

--- src/components/Stack.svelte
+++ src/components/Stack.svelte
@@ -5,18 +5,19 @@
     type Props = {
         gap?: Spacing;
         vertical?: boolean;
-        align?: "start" | "center" | "end" | "stretch"; 
+        align?: "start" | "center" | "end"; 
         children: Snippet;
     };
     
-    const { children, gap, vertical = false, align = "stretch" }: Props = $props();
+    const { children, gap, vertical = false, align }: Props = $props();
 </script>
 
 <div
     style:display="flex"
     style:gap={gap ? `var(--size-${gap})` : "0"}
     style:flex-direction={vertical ? "column" : "row"}
-    style:align-items={align}
+    style:align-items={vertical && align ? align : "stretch"}
+    style:justify-content={vertical ? "start" : align ?? "start"}    
     style:flex-wrap="wrap"
 >
     {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,38 @@
 </script>
 
 <Stack vertical gap="s">
+  <h4>stack vertical start</h4>
+  <Stack gap="xxs" vertical align="start">
+    <View padding="xxs" border background="green">element 1</View>
+    <View padding="xxs" border background="green">element 2</View>
+    <View padding="xxs" border background="green">element 3</View>
+  </Stack>
+  <h4>stack vertical center</h4>
+  <Stack gap="xxs" vertical align="center">
+    <View padding="xxs" border background="green">element 1</View>
+    <View padding="xxs" border background="green">element 2</View>
+    <View padding="xxs" border background="green">element 3</View>
+  </Stack>
+  <h4>stack vertical end</h4>
   <Stack gap="xxs" vertical align="end">
     <View padding="xxs" border background="green">element 1</View>
     <View padding="xxs" border background="green">element 2</View>
     <View padding="xxs" border background="green">element 3</View>
   </Stack>
-  <Stack gap="xxs">
+  <h4>stack horizontal start</h4>
+  <Stack gap="xxs" align="start">
+    <View padding="xxs" border background="green">element 1</View>
+    <View padding="xxs" border background="green">element 2</View>
+    <View padding="xxs" border background="green">element 3</View>
+  </Stack>
+  <h4>stack horizontal center</h4>
+  <Stack gap="xxs" align="center">
+    <View padding="xxs" border background="green">element 1</View>
+    <View padding="xxs" border background="green">element 2</View>
+    <View padding="xxs" border background="green">element 3</View>
+  </Stack>
+  <h4>stack horizontal end</h4>
+  <Stack gap="xxs" align="end">
     <View padding="xxs" border background="green">element 1</View>
     <View padding="xxs" border background="green">element 2</View>
     <View padding="xxs" border background="green">element 3</View>
